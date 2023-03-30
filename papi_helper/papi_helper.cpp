58ec5a4fa27b1eaff5af3ce2b40ba148587fd40d
Guilherme Costa <glhrmcosta91@gmail.com>
2023-03-31 00:38:03 +0100

    Allow disabling PAPI counters

--- papi_helper/papi_helper.cpp
+++ papi_helper/papi_helper.cpp
@@ -14,6 +14,7 @@ PapiHelper::PapiHelper()
 {
   int ret;
 
+#ifndef DISABLE_PAPI_COUNTERS
   this->EventSet = PAPI_NULL;
 
   ret = PAPI_library_init(PAPI_VER_CURRENT);
@@ -31,13 +32,14 @@ PapiHelper::PapiHelper()
   ret = PAPI_add_event(this->EventSet, PAPI_L2_DCM);
   if (ret != PAPI_OK)
     cout << "ERROR: PAPI_L2_DCM " << ret << endl;
-    
+#endif
 }
 
 PapiHelper::~PapiHelper()
 {
   int ret;
-
+  
+#ifndef DISABLE_PAPI_COUNTERS
   ret = PAPI_remove_event(this->EventSet, PAPI_L1_DCM);
   if (ret != PAPI_OK)
     std::cout << "FAIL remove event " << ret << endl;
@@ -51,35 +53,36 @@ PapiHelper::~PapiHelper()
   ret = PAPI_destroy_eventset(&this->EventSet);
   if (ret != PAPI_OK)
     std::cout << "FAIL destroy " << ret << endl;
-
+#endif
 }
 
 void PapiHelper::startCounting()
 {
   int ret;
 
+  this->countStarted = chrono::high_resolution_clock::now();
+
+#ifndef DISABLE_PAPI_COUNTERS
   ret = PAPI_reset(this->EventSet);
   if (ret != PAPI_OK)
     std::cout << "FAIL reset " << ret << endl;
-
-
-  this->countStarted = chrono::high_resolution_clock::now();
 
   // Start counting
   ret = PAPI_start(this->EventSet);
   if (ret != PAPI_OK)
     cout << "ERROR: Start PAPI " << ret << endl;
-
+#endif
 }
 
 void PapiHelper::stopCounting()
 {
   int ret;
 
+#ifndef DISABLE_PAPI_COUNTERS
   ret = PAPI_stop(this->EventSet, (long long*)this->counters);
   if (ret != PAPI_OK)
     cout << "ERROR: Stop PAPI " << ret << endl;
-
+#endif
 
   this->countStopped = chrono::high_resolution_clock::now();
 }
@@ -90,6 +93,8 @@ void PapiHelper::report()
 
   cout << "Performance Report:" << endl;
   cout << "\tTime: " <<  ellapsed_ms.count() << " ms"  << endl;
+#ifndef DISABLE_PAPI_COUNTERS
   cout << "\tL1 DCM: " << this->counters[L1_MISSES_IDX] << endl;
   cout << "\tL2 DCM: " << this->counters[L2_MISSES_IDX] << endl;
+#endif
 }
