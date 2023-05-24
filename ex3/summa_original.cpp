737c134570c4237bb306c6c62fcde0eef1884741
mafmagalhaes19 <mafi.magalhaes@gmail.com>
2023-05-24 21:57:10 +0100

    Results duplicated

--- ex3/summa_original.cpp
+++ ex3/summa_original.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <mpi.h>
-#include <cmath>
 
 void printMatrix(int* matrix, int rows, int cols) {
     for (int i = 0; i < rows; ++i) {
@@ -30,20 +29,17 @@ int main(int argc, char** argv) {
     // Assume A and B are square matrices of the same size
     int matrix_size = 8; // Size of each matrix
 
-    int size_squared = round(sqrt(size));
-    int submatrix_size = matrix_size / size_squared;
+    int submatrix_size = (matrix_size *matrix_size)/ size;
     int* matrix_A = nullptr;
     int* matrix_B = nullptr;
     int* submatrix_A = allocateMatrix(submatrix_size, submatrix_size);
     int* submatrix_B = allocateMatrix(submatrix_size, submatrix_size);
     int* submatrix_C = allocateMatrix(submatrix_size, submatrix_size);
-    int submatrix_size_squared = submatrix_size * submatrix_size;
-
-    // Initialize the original matrices A and B
-    matrix_A = allocateMatrix(matrix_size, matrix_size);
-    matrix_B = allocateMatrix(matrix_size, matrix_size);
 
     if (rank == 0) {
+        // Initialize the original matrices A and B
+        matrix_A = allocateMatrix(matrix_size, matrix_size);
+        matrix_B = allocateMatrix(matrix_size, matrix_size);
 
         for (int i = 0; i < matrix_size; ++i) {
             for (int j = 0; j < matrix_size; ++j) {
@@ -61,55 +57,24 @@ int main(int argc, char** argv) {
     }
 
     // Scatter the submatrices A and B to all processes
-    // MPI_Scatter(matrix_A, submatrix_size_squared, MPI_INT, submatrix_A, submatrix_size_squared, MPI_INT, 0, MPI_COMM_WORLD);
-    // MPI_Scatter(matrix_B, submatrix_size_squared, MPI_INT, submatrix_B, submatrix_size_squared, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Send the entire matrix to all processes
-    MPI_Bcast(matrix_A, matrix_size * matrix_size, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(matrix_B, matrix_size * matrix_size, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Extract the n square of matrix A and matrix B on each process
-    for (int i = 0; i < submatrix_size; i++) {
-        for (int j = 0; j < submatrix_size; j++) {
-
-            submatrix_A[i * submatrix_size + j] = matrix_A[(i + submatrix_size * (rank / size_squared)) * matrix_size + submatrix_size * (rank % size_squared) + j];
-            submatrix_B[i * submatrix_size + j] = matrix_B[(i + submatrix_size * (rank / size_squared)) * matrix_size + submatrix_size * (rank % size_squared) + j];
-            //submatrix_B[i * submatrix_size + j] = matrix_B[i * matrix_size + j];
-        }
-    }
+    MPI_Scatter(matrix_A, submatrix_size * matrix_size, MPI_INT, submatrix_A, submatrix_size * matrix_size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(matrix_B, matrix_size * submatrix_size, MPI_INT, submatrix_B, matrix_size * submatrix_size, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Perform SUMMA algorithm for matrix multiplication
     int* temp_matrix = allocateMatrix(submatrix_size, submatrix_size);
 
     for (int k = 0; k < size; ++k) {
         // Broadcast submatrix_A to all processes in the same row
-        MPI_Bcast(submatrix_A, submatrix_size_squared, MPI_INT, k, MPI_COMM_WORLD);
+        MPI_Bcast(submatrix_A, submatrix_size * submatrix_size, MPI_INT, k, MPI_COMM_WORLD);
 
         // Perform local matrix multiplication
         for (int i = 0; i < submatrix_size; ++i) {
             for (int j = 0; j < submatrix_size; ++j) {
                 temp_matrix[i * submatrix_size + j] = 0;
-                for (int p = 0; p < submatrix_size; ++p) {
-                    temp_matrix[i * submatrix_size + j] += submatrix_A[i * submatrix_size + p] * submatrix_B[p * submatrix_size + j];
+                for (int p = 0; p < matrix_size; ++p) {
+                    temp_matrix[i * submatrix_size + j] += submatrix_A[i * matrix_size + p] * submatrix_B[p * submatrix_size + j];
                 }
             }
-        }
-
-        if(rank == 1){
-            std::cout << "submatrix_A: " << std::endl;
-            std::cout << rank << std::endl;
-            printMatrix(submatrix_A, submatrix_size, submatrix_size);
-            std::cout << std::endl;
-
-            std::cout << "submatrix_B: " << std::endl;
-            std::cout << rank << std::endl;
-            printMatrix(submatrix_B, submatrix_size, submatrix_size);
-            std::cout << std::endl;
-
-            std::cout << "TempMatrix: " << std::endl;
-            std::cout << rank << std::endl;
-            printMatrix(temp_matrix, submatrix_size, submatrix_size);
-            std::cout << std::endl;
         }
 
         // Accumulate the results of local matrix multiplication into submatrix_C
@@ -122,15 +87,15 @@ int main(int argc, char** argv) {
         // Rotate submatrix_B in each row
         int source = (rank + 1) % size;
         int destination = (rank + size - 1) % size;
-        MPI_Sendrecv_replace(submatrix_B, submatrix_size_squared, MPI_INT, destination, 0, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Sendrecv_replace(submatrix_B, submatrix_size * submatrix_size, MPI_INT, destination, 0, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
 
     // Gather all submatrices C to the master process
     int* recv_counts = new int[size];
     int* displacements = new int[size];
     for (int i = 0; i < size; ++i) {
-        recv_counts[i] = submatrix_size_squared;
-        displacements[i] = submatrix_size_squared * i;
+        recv_counts[i] = submatrix_size * submatrix_size;
+        displacements[i] = i * submatrix_size * submatrix_size;
     }
 
     int* matrix_C = nullptr;
@@ -139,7 +104,7 @@ int main(int argc, char** argv) {
         matrix_C = allocateMatrix(matrix_size, matrix_size);
     }
 
-    MPI_Gatherv(submatrix_C, submatrix_size_squared, MPI_INT, matrix_C, recv_counts, displacements, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gatherv(submatrix_C, submatrix_size * submatrix_size, MPI_INT, matrix_C, recv_counts, displacements, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Print the final matrix C on the master process
     if (rank == 0) {
